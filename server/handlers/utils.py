929726000129385ba059e9be768753519c6689b4
Anton Bryzgalov <abryzgalov@optimum.omd.ru>
2019-01-30 20:35:12 +0300

    server.handlers.utils: alist removed

--- server/handlers/utils.py
+++ server/handlers/utils.py
@@ -5,8 +5,3 @@ class QueryTemplatesReplacer:
     def __call__(self, match):
         self._occurrence_no += 1
         return f'${self._occurrence_no}'
-
-
-async def alist(async_gen):
-    """ :type async_gen: typing.AsyncGenerator """
-    return [item async for item in async_gen]
